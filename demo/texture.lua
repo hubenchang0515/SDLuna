0add01f1ec4fdc4528282f158da4bc326cf1cb46
hubenchang0515 <hubenchang0515@outlook.com>
2018-11-30 15:27:06 +0800

    fix bug : function <LoadImage> leak memory of SDL_Surface

--- demo/texture.lua
+++ demo/texture.lua
@@ -2,26 +2,19 @@ local sdl = require("libsdluna")
 local window = sdl.CreateWindow("SDLuna", 
 								sdl.WINDOWPOS_UNDEFINED, 
 								sdl.WINDOWPOS_UNDEFINED,
-								1024, 768,
+								300, 300,
 								sdl.WINDOW_SHOWN)
 local renderer = sdl.CreateRenderer(window, -1, sdl.RENDERER_ACCELERATED)
-local texture = sdl.CreateTexture(renderer,
-									sdl.PIXELFORMAT_RGBA8888,
-									sdl.TEXTUREACCESS_TARGET,
-									1024, 768)
-math.randomseed(os.time())
+local texture = sdl.LoadImage(renderer, "logo.png")
+
 while true do
 	local event = sdl.PollEvent()
 	if event ~= nil and event.type == sdl.EVENT_QUIT then
 		break
 	end
 	
-	sdl.SetRenderTarget(renderer, texture)
-	sdl.SetRenderDrawColor(renderer, 0,0,0,0)
+	sdl.SetRenderDrawColor(renderer, 0xff,0xff,0xff,0)
 	sdl.RenderClear(renderer)
-	sdl.SetRenderDrawColor(renderer, 255,0,0,0)
-	sdl.FillRect(renderer, {100, 50, math.random(500), math.random(500)})
-	sdl.SetRenderTarget(renderer, nil)
 	sdl.RenderCopy(renderer, texture)
 	sdl.RenderPresent(renderer)
 end
--- src/sdluna_image.cpp
+++ src/sdluna_image.cpp
@@ -13,6 +13,6 @@ SDL_Texture* SDLuna_LoadImage(SDL_Renderer* renderer, const char* file)
 {
 	SDL_Surface* surface = IMG_Load(file);
     SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer,surface);
-	
+	SDL_FreeSurface(surface);
 	return texture;
 }
